373efa435528e117d4302c8e7a1e5754b8e0eb80
xdevl <xdevl@users.noreply.github.com>
2019-03-30 18:07:53 +0000

    Migrated to JSZip library to read and extract APK file entries

--- lib/apk.ts
+++ lib/apk.ts
@@ -3,45 +3,39 @@
 This work is licensed under the terms of the MIT license.
 For a copy, see <https://opensource.org/licenses/MIT>.*/
 
-import NodeZip from "yauzl-promise";
+import NodeFs from "fs";
+import NodeZip from "jszip";
 import BinaryXml from "./binaryXml";
 import Certificate from "./certificate";
 import Source from "./source";
 
 export default class Apk {
   private path: string;
-  private promise: Promise<NodeZip.ZipFile>;
 
   constructor(path: string) {
     this.path = path;
-    this.promise = NodeZip.open(path);
   }
 
-  public lookupEntry(key: string): Promise<NodeZip.Entry> {
-    return new Promise((resolve, reject) => {
-      let found: NodeZip.Entry;
-      this.promise.then((zip) => zip.walkEntries((entry) => {
-        if (entry.fileName === key) {found = entry; }
-      })).then(() => found !== undefined ? resolve(found) : reject("Entry not found: " + key));
-    });
+  public lookupEntry(key: string): Promise<NodeJS.ReadableStream> {
+    return this.bufferize(NodeFs.createReadStream(this.path))
+      .then((buffer) => NodeZip.loadAsync(buffer))
+      .then((zip) => zip.files[key].nodeStream());
   }
 
   public getCertificateInfo(): Promise<Certificate[]> {
     return this.lookupEntry("META-INF/CERT.RSA")
-      .then((entry) => entry.openReadStream())
       .then((stream) => this.bufferize(stream))
       .then((buffer) => Certificate.parse(buffer));
   }
 
   public getManifestInfo(): Promise<BinaryXml> {
     return this.lookupEntry("AndroidManifest.xml")
-      .then((entry) => entry.openReadStream())
       .then((stream) => this.bufferize(stream))
       .then((buffer) => new BinaryXml(new Source(buffer)));
   }
 
   public close() {
-    return this.promise.then((zip) => zip.close());
+    return undefined;
   }
 
   private bufferize(stream: NodeJS.ReadableStream): Promise<Buffer> {
